3963d0cc2e0116946c8df1f6428798aafb8b5a16
Jason Dent <jason@streetsidesoftware.nl>
2019-01-26 15:09:41 +0100

    Add a string to utf8 method.

---
+++ src/toUtf8.test.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import toUtf8 from './toUtf8';
+import {toUtf8_1, toUtf8_2, toUtf8_3} from './toUtf8';
+import * as lorem from 'lorem-ipsum';
+
+const units: ('words' | 'sentences' | 'paragraphs')[] = ['words', 'sentences', 'paragraphs'];
+
+describe('Validate toUtf8', () => {
+    getSamples().forEach(s =>
+        it(`Test string: "${s.replace(/^(.{20}).*$/, '$1...')}"`, () => {
+            const expected = Buffer.from(s, 'utf8');
+            const actual = toUtf8(s);
+            expect(actual).to.deep.equal(expected);
+        })
+    );
+
+    for (let i = 0; i < 0; ++i) {
+        const text = lorem({
+            count: Math.floor(Math.random() * 20),
+            units: units[i % 3],
+        });
+        it(`Test random string: "${text.slice(0, 30).replace(/^(.{20}).*$/, '$1...')}"`, () => {
+            const expected = Buffer.from(text, 'utf8');
+            const actual = toUtf8(text);
+            expect(actual).to.deep.equal(expected);
+            expect(toUtf8_1(text)).to.deep.equal(expect);
+            expect(toUtf8_2(text)).to.deep.equal(expect);
+            expect(toUtf8_3(text)).to.deep.equal(expect);
+        });
+    }
+});
+
+function getSamples() {
+    return [
+        'a',
+        'á ä £ ™ ¢',
+        '',
+        'नी ›',
+        'My text to hash 😊',
+    ];
+}
---
+++ src/toUtf8.ts
@@ -0,0 +1,79 @@
+export function toUtf8_1(text: string): Uint8Array {
+    const bytes: number[] = [];
+    const w = new Array<number>(4);
+    const h = [0x00, 0xc0, 0xe0, 0xf0];
+    const m = [0x7f, 0x3f, 0x3f, 0x3f];
+    const p = [0x00, 0x80, 0x80, 0x80];
+    for (const char of text) {
+        const b = w;
+        const cp = char.codePointAt(0)!;
+
+        const n = 0
+                 - ((-(cp & 0xffffff80) >> 31))
+                 - ((-(cp & 0xfffff800) >> 31))
+                 - ((-(cp & 0xffff0000) >> 31));
+
+        const z = m[n];
+        const y = p[n];
+        b[3] = y | (cp & z);
+        b[2] = y | ((cp >>>  6) & z);
+        b[1] = y | ((cp >>> 12) & z);
+        b[0] = y | ((cp >>> 18) & z);
+        const s = 3 - n;
+        b[s] |= h[n];
+
+        Array.prototype.push.apply(bytes, b.slice(s));
+    }
+    return new Uint8Array(bytes);
+}
+
+export function toUtf8_2(text: string): Uint8Array {
+    const bytes: number[] = [];
+    for (const char of text) {
+        const cp = char.codePointAt(0)!;
+        if (cp < 0x80) {
+            bytes.push(cp);
+        } else if (cp < 0x800) {
+            bytes.push(0xC0 | ((cp >> 6) & 0x1f), 0x80 | cp & 0x3f);
+        } else if (cp < 0x10000) {
+            bytes.push(0xe0 | ((cp >> 12) & 0xf), 0x80 | (cp >> 6) & 0x3f, 0x80 | cp & 0x3f);
+        } else {
+            bytes.push(0xf0 | ((cp >> 18) & 0x7), 0x80 | (cp >> 12) & 0x3f, 0x80 | (cp >> 6) & 0x3f, 0x80 | cp & 0x3f);
+        }
+    }
+    return new Uint8Array(bytes);
+}
+
+/**
+ * Convert text to UTF-8 byte array.
+ * @param text text to be converted to utf-8 bytes
+ * Note: this one seems to be the fastest based upon perf tests.
+ */
+export function toUtf8_3(text: string): Uint8Array {
+    const bytes: number[] = [];
+    for (let i = 0, n = text.length; i < n; ++i) {
+        const c = text.charCodeAt(i);
+        if (c < 0x80) {
+            bytes.push(c);
+        } else if (c < 0x800) {
+            bytes.push(
+                0xC0 | c >> 6,
+                0x80 | c & 0x3f);
+        } else if (c < 0xd800 || c >= 0xe000) {
+            bytes.push(
+                0xe0 | c >> 12,
+                0x80 | (c >> 6) & 0x3f,
+                0x80 | c & 0x3f);
+        } else {
+            let cp = 0x10000 + ((c & 0x3ff) << 10 | (text.charCodeAt(++i) & 0x3ff));
+            bytes.push(
+                0xf0 | ((cp >> 18) & 0x7),
+                0x80 | (cp >> 12) & 0x3f,
+                0x80 | (cp >> 6) & 0x3f,
+                0x80 | cp & 0x3f);
+        }
+    }
+    return new Uint8Array(bytes);
+}
+
+export default toUtf8_3;
